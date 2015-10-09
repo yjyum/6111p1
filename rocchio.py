a01aa89b2589db51a69bc212d6d61c3c395fadc6
yjyum <oreo_yjy@hotmail.com>
2015-10-09 08:42:33 -0400

    refactor rocchio.py

--- rocchio.py
+++ rocchio.py
@@ -12,6 +12,7 @@ class rocchio:
         self.descriptionidf = {}
         return
 
+    # compute new query using rocchio algorithms
     def compute(self, queryList, documents):
         relevantDocNum = 0
         for url, doc in documents.iteritems():
@@ -22,17 +23,11 @@ class rocchio:
         #compute idf
         for url, doc in documents.iteritems():
             for word in doc.titleTF.keys():
-                if word in self.titleidf.keys():
-                    self.titleidf[word]=self.titleidf[word]+1
-                else:
-                    self.titleidf[word]=1
+                addToMap(self.titleidf, word, 1)
             for word in self.titleidf.keys():
                 self.titleidf[word]=math.log(len(documents)/self.titleidf[word])
             for word in doc.descriptionTF.keys():
-                if word in self.descriptionidf.keys():
-                    self.descriptionidf[word]=self.descriptionidf[word]+1
-                else:
-                    self.descriptionidf[word]=1
+                addToMap(self.descriptionidf, word, 1)
             for word in self.descriptionidf.keys():
                 self.descriptionidf[word]=math.log(len(documents)/self.descriptionidf[word])
 
@@ -43,27 +38,21 @@ class rocchio:
                 temp = -self.gamma/(totalDocNum-relevantDocNum)
 
             for word in doc.titleTF.keys():
-                if word in self.weight.keys():
-                    self.weight[word] = self.weight[word] + temp*(doc.titleTF[word]*self.titleidf[word])
-                else:
-                    self.weight[word] = temp*(doc.titleTF[word]*self.titleidf[word])
+                addToMap(self.weight, word, temp*(doc.titleTF[word]*self.titleidf[word]))
 
             for word in doc.descriptionTF.keys():
-                if word in self.weight.keys():
-                    self.weight[word] = self.weight[word] + temp*(doc.descriptionTF[word]*self.descriptionidf[word])
-                else:
-                    self.weight[word] = temp*(doc.descriptionTF[word]*self.descriptionidf[word])
+                addToMap(self.weight, word, temp*(doc.descriptionTF[word]*self.descriptionidf[word]))
 
         for query in queryList:
-            if query in self.weight.keys():
-                self.weight[query] = self.weight[query] + self.alpha
-            else:
-                self.weight[query] = self.alpha
+            addToMap(self.weight, query, self.alpha)
             # delete similar words of query generated after stemming
             if stem(query)!=query and stem(query) in self.weight.keys():
                 self.weight[query] = self.weight[query]+self.weight[stem(query)]
                 del self.weight[stem(query)]
+        # debug
+        # print sorted(self.weight.items(), key=operator.itemgetter(1))
 
+        # find one new query which is of the max weight
         newQuery = ""
         maxScore = float("-inf")
         for term in self.weight.keys():
@@ -72,21 +61,22 @@ class rocchio:
                     newQuery = term
                     maxScore = self.weight[term] 
 
+        # change new query from root to most frequect form in docs
         oriQuery = {}
         for url, doc in documents.iteritems():
-            if newQuery in doc.titleWordPos.keys():
-                for pos in doc.titleWordPos[newQuery]:
-                    if newQuery in oriQuery.keys():
-                        oriQuery[doc.noStemTitleWordList[pos]] += 1
-                    else:
-                        oriQuery[doc.noStemTitleWordList[pos]] = 1
-            if newQuery in doc.descriptionWordPos.keys():
-                for pos in doc.descriptionWordPos[newQuery]:
-                    if newQuery in oriQuery.keys():
-                        oriQuery[doc.noStemDescriptionWordList[pos]] += 1
-                    else:
-                        oriQuery[doc.noStemDescriptionWordList[pos]] = 1
-
+            if doc.relevant:
+                if newQuery in doc.titleWordPos.keys():
+                    for pos in doc.titleWordPos[newQuery]:
+                        addToMap(oriQuery, doc.noStemTitleWordList[pos], 1)
+                if newQuery in doc.descriptionWordPos.keys():
+                    for pos in doc.descriptionWordPos[newQuery]:
+                        addToMap(oriQuery, doc.noStemDescriptionWordList[pos], 1)
         newQuery = max(oriQuery.iteritems(), key=operator.itemgetter(1))[0]
 
         return newQuery
+
+def addToMap(map, key, value):
+    if key in map.keys():
+        map[key] += value
+    else:
+        map[key] = value
